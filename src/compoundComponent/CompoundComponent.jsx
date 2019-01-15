559e4c1ef51085648d9ea0263f26592e98f1f1c9
Camilo Colorado <camilo.colorado@globant.com>
2019-01-15 16:23:16 -0500

    initial component

---
+++ src/compoundComponent/CompoundComponent.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class CompoundComponent extends React.Component{
+
+    constructor(){
+        super();
+    }
+
+    render(){
+        return <h1>Hi I'm CompoundComponent</h1>
+    }
+}
+
+export default CompoundComponent;
